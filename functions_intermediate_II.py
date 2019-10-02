5b2e1f2e87413a38ac3f1f08a3b623964664ee07
NewmanScott <55299190+NewmanScott@users.noreply.github.com>
2019-10-02 09:31:14 -0400

    Add files via upload

---
+++ functions_intermediate_II.py
@@ -0,0 +1,80 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = 30
+
+def iterateDictionary(list):
+    for i in range(len(list)):
+        for y in list[i]:
+            if y == 'first_name':
+                print("first_name -",list[i][y])
+            elif y == 'last_name':
+                print("last_name -", list[i][y]) 
+
+def iterateDictionary2(val, list):
+    for i in range(len(list)):
+        print(list[i][val])
+
+
+            
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+#iterateDictionary(students)
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+first_name - Michael, last_name - Jordan
+first_name - John, last_name - Rosales
+first_name - Mark, last_name - Guillen
+first_name - KB, last_name - Tonel
+
+
+def printInfo(some_dict):
+    for y in some_dict:
+        print(len(some_dict[y]), y)
+        for i in range(0,len(some_dict[y]),1):
+            print(some_dict[y][i])
+        
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+# output:
+7 LOCATIONS
+San Jose
+Seattle
+Dallas
+Chicago
+Tulsa
+DC
+Burbank
+    
+8 INSTRUCTORS
+Michael
+Amy
+Eduardo
+Josh
+Graham
+Patrick
+Minh
+Devon
